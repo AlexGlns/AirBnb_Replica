f1a0708084c75c4950c1b24c16f05fead6dc619a
gerpapa <gerasimospapd@gmail.com>
2023-08-19 13:40:25 +0300

    readme

--- backend/backend_app/urls.py
+++ backend/backend_app/urls.py
@@ -17,10 +17,15 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # creates user
     path('users/create/', CustomUserCreateView.as_view(), name='user-create'),
+    # creates property --needs already created user
     path('properties/create/', PropertyListCreateView.as_view(), name='property-list'),
+    # returns info on property with certain id
     path('properties/<int:pk>/', PropertyDetailView.as_view(), name='property-detail'),
+    # creates reservations --needs already created propertty to base the reservation
     path('reservations/create/', ReservationListCreateView.as_view(), name='reservation-list'),
+    # returns info on reservation with certain id 
     path('reservations/<int:pk>/', ReservationDetailView.as_view(), name='reservation-detail'),
     # get functions for info in json format
     path('reservations/<int:reservation_id>/info/', get_reservation_info, name='reservation-info'),
