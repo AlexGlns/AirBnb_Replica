7fede0fec9f8c24c807dbb2553d8e2997ecede4c
gerpapa <gerasimospapd@gmail.com>
2023-09-29 19:20:27 +0300

    id

--- backend/backend_core/settings.py
+++ backend/backend_core/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
